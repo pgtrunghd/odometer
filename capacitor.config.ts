63f128bc7db4ee36dd263830241a0eaf1e7883f2
Phạm Gia Trung <phamgiatrung@Mac-mini-cua-Pham.local>
2026-03-11 21:51:56 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'odometer',
+  webDir: 'www'
+};
+
+export default config;
